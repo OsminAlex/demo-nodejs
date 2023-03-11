6f683b0a1d7b9b6b0ae0dce3f6db6d0f9fe9cc87
Osmin A. Mendoza Sevila <2012617@lab.ing.edu.ni>
2023-03-11 09:46:56 -0600

    Creada nueva rutaBascia

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ const app = express();
 
 const PORT = 4000;
 
+app.get("/", (res, req)  => {
+    res.send("Hola");
+})
+
 app.listen(PORT, () => {
     console.log(`Aplicación corriendo en el puerto ${PORT}`)
 })
